bf247c4e9ccc0eac33d47c826858ac35922efbec
Pablo F. Moura <pablo.f.moura17@gmail.com>
2026-01-11 16:58:21 -0300

    🚧 Construction: Development in progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Board } from "./components/Board";
+import { useState } from "react";
+import { BoardList } from "./components/BoardList";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Divider, Typography } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedBoardId, setSelectedBoardId] = useState<string | null>(null);
+
+  console.log("selectedBoardId: ", selectedBoardId);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <CssBaseline />
+      <div
+        style={{
+          width: "100vw",
+          // border: "10px solid red",
+          height: "100vh",
+          display: "flex",
+          maxWidth: "100vw",
+          maxHeight: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            variant: "h1",
+            padding: "20px",
+            fontSize: "24px",
+            fontWeight: "bold",
+            // border: "10px solid red",
+          }}
+        >
+          Mini Kanban Portal
+        </Typography>
+        <Divider sx={{ borderBottomWidth: "2px", mx: "12px" }} />
+
+        <BoardList
+          onSelectBoard={setSelectedBoardId}
+          selectedBoardId={selectedBoardId}
+        />
+
+        <Divider sx={{ borderBottomWidth: "2px", mx: "12px" }} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,3 +1,52 @@
-export function BoardList() {
-  return <div>BoardList</div>;
+import { useEffect } from "react";
+import { getBoards } from "../api/boards";
+import { useKanbanStore } from "../store/kanbanStore";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+
+interface Props {
+  onSelectBoard: (id: string) => void;
+  selectedBoardId: string | null;
+}
+
+export function BoardList({ onSelectBoard, selectedBoardId }: Props) {
+  const boards = useKanbanStore((s) => s.boards);
+
+  const setBoards = useKanbanStore((s) => s.setBoards);
+
+  useEffect(() => {
+    getBoards().then((res) => {
+      if (res?.data && Array.isArray(res.data)) {
+        setBoards(res.data);
+        if (selectedBoardId === null && res.data.length > 0) {
+          onSelectBoard(res.data?.[0]?.id);
+        }
+      } else {
+        setBoards([]);
+      }
+    });
+  }, [setBoards]);
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="board-select-label">Quadro</InputLabel>
+        <Select
+          labelId="board-select-label"
+          id="board-select"
+          value={selectedBoardId || ""}
+          label="Quadro"
+          onChange={(e) => onSelectBoard(e.target.value)}
+        >
+          {boards.map((board) => (
+            <MenuItem key={board.id} value={board.id}>
+              {board.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
