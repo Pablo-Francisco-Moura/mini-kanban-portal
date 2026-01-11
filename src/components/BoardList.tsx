bb856deebbf7a613d969ce1a58d81f0f5bf38b52
Pablo F. Moura <pablo.f.moura17@gmail.com>
2026-01-11 17:58:22 -0300

    🚧 Construction: Create modal to new board.

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,7 +1,18 @@
-import { useEffect } from "react";
-import { getBoards } from "../api/boards";
+import {
+  Dialog,
+  Button,
+  Tooltip,
+  TextField,
+  IconButton,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import { useKanbanStore } from "../store/kanbanStore";
+import { useEffect, useState } from "react";
+import { getBoards, createBoard } from "../api/boards";
 import Select from "@mui/material/Select";
+import AddIcon from "@mui/icons-material/Add";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
@@ -13,8 +24,26 @@ interface Props {
 
 export function BoardList({ onSelectBoard, selectedBoardId }: Props) {
   const boards = useKanbanStore((s) => s.boards);
-
   const setBoards = useKanbanStore((s) => s.setBoards);
+
+  const [open, setOpen] = useState(false);
+  const [newBoardName, setNewBoardName] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const createNewBoard = async () => {
+    setLoading(true);
+    try {
+      const res = await createBoard({ name: newBoardName });
+      if (res?.data) {
+        setBoards([...boards, res.data]);
+        setNewBoardName("");
+        setOpen(false);
+        onSelectBoard(res?.data?.id);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     getBoards().then((res) => {
@@ -34,10 +63,10 @@ export function BoardList({ onSelectBoard, selectedBoardId }: Props) {
       <FormControl sx={{ m: 1, width: 300 }}>
         <InputLabel id="board-select-label">Quadro</InputLabel>
         <Select
-          labelId="board-select-label"
           id="board-select"
           value={selectedBoardId || ""}
           label="Quadro"
+          labelId="board-select-label"
           onChange={(e) => onSelectBoard(e.target.value)}
         >
           {boards.map((board) => (
@@ -47,6 +76,38 @@ export function BoardList({ onSelectBoard, selectedBoardId }: Props) {
           ))}
         </Select>
       </FormControl>
+      <Tooltip arrow title="Adicionar Quadro">
+        <IconButton onClick={() => setOpen(true)}>
+          <AddIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Novo Quadro</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Nome do Quadro"
+            value={newBoardName}
+            margin="dense"
+            onChange={(e) => setNewBoardName(e.target.value)}
+            disabled={loading}
+            autoFocus
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} disabled={loading}>
+            Cancelar
+          </Button>
+          <Button
+            onClick={createNewBoard}
+            disabled={!newBoardName.trim() || loading}
+            variant="contained"
+          >
+            Criar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
